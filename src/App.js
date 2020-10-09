fd4ab1a4af7b79ca8daad6bb5a9a61ca3030686c
Chad Lusseni <chadlusseni@Chads-MacBook-Pro.local>
2020-10-09 12:12:49 +1100

    add context and reducer to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import logo from './svg/logo.svg';
-import './App.css';
+import React from 'react'
+import { useImmerReducer } from 'use-immer'
+import './App.css'
+
+/**
+ * -- CONTEXT --
+ */
+import DispatchContext from './context/DispatchContext'
+import StateContext from './context/StateContext'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+	/**
+	 * -- APP WIDE REDUCER --
+	 */
+	const initialState = {
+		someState: ''
+	}
+	function reducer(draft, action) {
+		switch (action.type) {
+			case 'someType':
+				draft.someState = action.value
+				return
+
+			default:
+				return
+		}
+	}
+	const [state, dispatch] = useImmerReducer(reducer, initialState)
+
+	return (
+		<DispatchContext.Provider value={dispatch}>
+			<StateContext.Provider value={state}>
+				<div className='App'>HELLO</div>
+			</StateContext.Provider>
+		</DispatchContext.Provider>
+	)
 }
 
-export default App;
+export default App
